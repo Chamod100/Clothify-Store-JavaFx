9049fd417e971aed0b91da0e89ce6fb9fe5d369f
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-26 23:59:47 +0530

    add repositoryImpl

---
+++ src/main/java/edu/icet/repository/custom/impl/AuthDaoImpl.java
@@ -0,0 +1,43 @@
+package edu.icet.repository.custom.impl;
+
+import com.clothify.db.DBConnection;
+import com.clothify.entity.UserCredentialsEntity;
+import com.clothify.repository.AuthDao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDateTime;
+
+public class AuthDaoImpl implements AuthDao {
+    @Override
+    public UserCredentialsEntity getUserByEmail(String email) {
+        String SQl = "SELECT * FROM user_credentials WHERE email=?";
+
+        try {
+            Connection connection = DBConnection.getInstance().getConnection();
+            PreparedStatement psTm = connection.prepareStatement(SQl);
+            psTm.setString(1, email );
+
+            ResultSet rs = psTm.executeQuery();
+
+            UserCredentialsEntity userCredentials = null;
+            while (rs.next()) {
+                String timeDate = rs.getString(6);
+                timeDate = timeDate.replace(' ', 'T');
+                userCredentials = new UserCredentialsEntity(
+                        rs.getString(1),
+                        rs.getString(2),
+                        rs.getString(3),
+                        rs.getString(4),
+                        rs.getString(5),
+                        LocalDateTime.parse(timeDate));
+            }
+            return userCredentials;
+        }catch (SQLException e) {
+            System.out.println(e.getMessage());
+            return null;
+        }
+    }
+}
