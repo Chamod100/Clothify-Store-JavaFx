476e90d873b2b3d44ec3ea2d994e79239a2f4fd8
CHAMOD RATHNAYAKA <chamodrathnayaka008@gmail.com>
2025-12-26 23:58:08 +0530

    ada DTO model with Lombok annotations

---
+++ src/main/java/edu/icet/model/dto/Employee.java
@@ -0,0 +1,19 @@
+package edu.icet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Employee {
+    private String id;
+    private String title;
+    private String name;
+    private String position;
+    private String address;
+    private String phoneNumber;
+    private String email;
+    public Boolean loginAccess;
+}
---
+++ src/main/java/edu/icet/model/dto/UserCredentials.java
@@ -0,0 +1,19 @@
+package edu.icet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserCredentials {
+    private String id;
+    private String employeeID;
+    private String email;
+    private String password;
+    private String role;
+    private LocalDateTime createdAt;
+}
